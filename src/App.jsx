34396b6cf93f57205f99f0e7cf8f76668f8aeac3
Claudio <claudio.westerik@gmail.com>
2025-04-01 15:14:24 +0200

    white space button- Gym streak Tracker

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,8 @@ export default function App() {
   };
 
   return (
-    <div className="p-4 max-w-sm mx-auto text-center">
+    <div style={{ display:"flex", flexDirection: "column", gap: "1rem"}}>
+    //<div className="p-4 max-w-sm mx-auto text-center">
       <Card>
         <CardContent>
           <h2 className="text-xl font-bold mb-2">🏋️ Gym Streak</h2>
